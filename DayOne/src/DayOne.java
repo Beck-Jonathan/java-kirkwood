9247d04f3fd7a337346b588770aecdb7fd1e05d0
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-01-24 18:42:54 -0600

    fix git ignore issue

---
+++ DayOne/src/DayOne.java
@@ -0,0 +1,9 @@
+/*
+   This is a simple Java program.
+ */
+public class DayOne {
+    // A Java program begins with a call to main().
+    public static void main(String args[]) {
+        System.out.println("Java drives the Web.");
+    }
+}
