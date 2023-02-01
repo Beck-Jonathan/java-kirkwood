3a1d5689443e18e53496c47c3482af2ad208ad06
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-01-31 18:23:45 -0600

    merge

---
+++ DayTwo/src/DayTwo.java
@@ -0,0 +1,9 @@
+public class DayTwo {
+    // A Java program begins with a call to main().
+    public static void main(String args[]) {
+            double weight_on_earth = 155;
+            double weight_on_moon;
+            weight_on_moon=weight_on_earth/6;
+    System.out.println(weight_on_moon);
+        }
+    }
