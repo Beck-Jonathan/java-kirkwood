1a2785b7d81efc0ff115c8ecec1c588b1631a60f
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-07 20:40:24 -0600

    chapter 2 notes

--- Chapter2/src/Feb_6.java
+++ Chapter2/src/Feb_6.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Feb_6 {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
@@ -5,8 +7,31 @@ public class Feb_6 {
             if (i % 2 == 0) {
                 System.out.print(" is even");
             } else {
-                System.out.print(" is odd");}
-            System.out.println("");
+                System.out.print(" is odd");
+            }
+            System.out.println();
         }
+        // talked about truth tables
+        // && and || are short circuits, they evaluate the first one, then make appropriate decisions,
+        //sometimes skipping the second evaluation
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter a number:");
+        double num1 = keyboard.nextDouble();
+        System.out.println("enter another number");
+        double num2 = keyboard.nextDouble();
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        if (num2 !=0) {
+            System.out.println(num1 / num2);
+            System.out.println(num1 % num2);
+        }
+        if (num2 !=0 && num1 % num2 ==0){
+            System.out.println(num2 +" is a factor of " +num1);
+        }
+
+
+
     }
 }
+
