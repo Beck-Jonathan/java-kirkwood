2eb80fac51ae3d0d67095d3d7484630e613ee819
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-07 20:16:11 -0600

    chapter 2 notes

---
+++ Chapter2/src/Feb_6.java
@@ -0,0 +1,12 @@
+public class Feb_6 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i);
+            if (i % 2 == 0) {
+                System.out.print(" is even");
+            } else {
+                System.out.print(" is odd");}
+            System.out.println("");
+        }
+    }
+}
--- PartnerAssignments/src/Assignment1_part1.java
+++ PartnerAssignments/src/Assignment1_part1.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
+
 //Jonathan was the Driver, Keaton was the navigator
 public class Assignment1_part1 {
-public static void main(String[]args){
-    Scanner getter = new Scanner(System.in);
-    System.out.println("Please enter a number:");
-    double a = getter.nextDouble();
-    System.out.println("Please enter a number:");
-    double b = getter.nextDouble();
-    System.out.print("The Larger number is: ");
-    if (a>=b) {
-        System.out.println(a);
-    } else System.out.println(b);
-}
+    public static void main(String[] args) {
+        Scanner getter = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        double a = getter.nextDouble();
+        System.out.println("Please enter a number:");
+        double b = getter.nextDouble();
+        System.out.print("The Larger number is: ");
+        if (a >= b) {
+            System.out.println(a);
+        } else System.out.println(b);
+    }
 
-        }
+}
 
