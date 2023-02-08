a77810f8020811ee118626ded1304fe026e10bbc
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-07 21:10:02 -0600

    chapter 2 notes

--- Chapter2/src/Feb_6.java
+++ Chapter2/src/Feb_6.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Feb_6 {
@@ -19,19 +20,55 @@ public class Feb_6 {
         double num1 = keyboard.nextDouble();
         System.out.println("enter another number");
         double num2 = keyboard.nextDouble();
+
         System.out.println(num1 + num2);
         System.out.println(num1 - num2);
         System.out.println(num1 * num2);
-        if (num2 !=0) {
+        if (num2 != 0) {
             System.out.println(num1 / num2);
             System.out.println(num1 % num2);
         }
-        if (num2 !=0 && num1 % num2 ==0){
-            System.out.println(num2 +" is a factor of " +num1);
+        if (num2 != 0 && num1 % num2 == 0) {
+            System.out.println(num2 + " is a factor of " + num1);
         }
-
-
-
+//castiang pages 55 to 57
+        int n = 4;
+        double m = n;
+        System.out.println(m);
+        double k = 6.3;
+        int j = (int) k; //"casts" double to int, which loses data, but is the only way to get this double here
+        System.out.println(j); //prints 6
+        //print 10 random numbers between 1 and 6
+        for (int x = 0; x < 10; x++) {
+            int dice = (int) (Math.random() * 6) + 1;
+            System.out.print(dice + " ");
+        }
+        System.out.println();
+        //generate 10 random numbers between -10 and 10
+        for (int x = 0; x < 10; x++) {
+            int dice = (int) (Math.random() * 20) - 10;
+            System.out.print(dice + " ");
+        }
+        System.out.println();
+        //generate 30 random numbers between -10 and 10
+        Random random = new Random();
+        int min = -10;
+        int max = 10;
+        for (int i = 0; i < 30; i++) {
+            int randomNumber = random.nextInt(max + 1 - min) + min;
+            System.out.print(randomNumber + " ");
+        }
+        System.out.println();
+        for (int i = 65; i <= 90; i++) {
+            System.out.println((char) i);
+        }
+        for (int i =1;i<=10; i++){
+            System.out.printf("%d /3 = %.3f\n",i,(double) i / 3);
+        }
     }
 }
+
+
+
+
 
