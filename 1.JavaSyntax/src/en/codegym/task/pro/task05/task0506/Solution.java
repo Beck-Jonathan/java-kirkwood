7e689634c8b1572f310cf129680772e89ecd6b65
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-14 09:14:01 -0600

    codegym 5 complete

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task05/task0506/Solution.java
@@ -0,0 +1,28 @@
+package en.codegym.task.pro.task05.task0506;
+
+import java.util.Scanner;
+
+/* 
+Minimum of N numbers
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) {
+        Scanner getter = new Scanner(System.in);
+        int n=getter.nextInt();
+        array = new int [n];
+        for (int i=0;i<n;i++){
+            array[i]=getter.nextInt();
+        }
+        int x =array.length;
+        int min =99999;
+        for (int y=0;y<x;y++){
+            if (array[y]<min){
+                min = array[y];
+            }
+        }
+        System.out.println(min);
+    }
+}
---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task05/task0507/Solution.java
@@ -0,0 +1,28 @@
+package en.codegym.task.pro.task05.task0507;
+
+import java.util.Scanner;
+
+/* 
+Maximum of N numbers
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) {
+        Scanner getter = new Scanner(System.in);
+        int n = getter.nextInt();
+        array = new int[n];
+        int max = -10000;
+        for (int i = 0; i < n; i++) {
+            array[i] = getter.nextInt();
+        }
+        for (int y = 0; y < n; y++) {
+            if (array[y] > max) {
+                max = array[y];
+            }
+        }
+        System.out.println(max);
+    }
+}
+
