c5757322cdbb7cc86339ff426717c3ffa49d299c
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-14 18:05:03 -0600

    rearrange folders

---
+++ CourseDemos/src/Feb_14.java
@@ -0,0 +1,15 @@
+public class Feb_14 {
+   public static void main(String[] args) {
+      System.out.println("test");
+      char test = 'J';
+      String test1 = "Feb";
+      if (test =='j'){
+         System.out.println("letter success");}
+      if (test1 == "Feb"){
+         System.out.println("string success");
+      }
+      //chapter 3 Lops and stuff, lots of stuff here
+      
+
+   }
+}
