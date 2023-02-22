64c480caf63cee086b1c0a093e832c988e3f1a2c
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-21 19:19:52 -0600

    Partner Assignmetn 2

--- CourseDemos/PartnerAssignments/src/Assignment2_part1.java
+++ CourseDemos/PartnerAssignments/src/Assignment2_part1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Assignment2_part1 {
     public static void main(String[] args) {
-        //prompt user for  number, answer if even, odd, or decimal, which is neither
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double user_input=scanner.nextDouble();
+        if (user_input%2==0) {
+            System.out.println(user_input + " is even");
+        }else if(user_input%2==1){
+            System.out.println(user_input + " is odd");
+        }else {
+            System.out.println(user_input+" is neither odd nor even");
+            
+        }
     }
 }
--- CourseDemos/PartnerAssignments/src/Assignment2_part2.java
+++ CourseDemos/PartnerAssignments/src/Assignment2_part2.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Assignment2_part2 {
     public static void main(String[] args) {
-        //number of seconds to days to hours minutes seconds
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entre a number for seconds: ");
+        long seconds = scanner.nextLong();
+        long days = seconds / (60 * 60 * 24);
+        long leftOvers = seconds % (60 * 60 * 24);
+        long hours = leftOvers / (60 * 60);
+        leftOvers = leftOvers % (60 * 60);
+        long minutes = leftOvers / 60;
+        seconds = leftOvers % 60;
+
+        String daysPlural = (days == 1)? "":"s";
+        String hoursPlural = (hours == 1)? "":"s";
+        String secondsPlural = (seconds == 1)? "":"s";
+        String minutesPlural = (minutes == 1)? "":"s";
+
+        System.out.println("Day"+ daysPlural + " " + days);
+        System.out.println("Hour"+ hoursPlural + " " + hours);
+        System.out.println("Minute"+ minutesPlural + " " + minutes);
+        System.out.println("Second"+ secondsPlural + " " + seconds);
     }
 }
