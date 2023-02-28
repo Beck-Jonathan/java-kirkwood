d90f3b688f84c7eda8afcfc2da1147453f89f3fb
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-28 17:51:06 -0600

    Feb 28 warm up

---
+++ CourseDemos/src/chapter_5_6/feb_28.java
@@ -0,0 +1,14 @@
+package chapter_5_6;
+
+public class feb_28 {
+    public static void main(String[] args) {
+        int a=21;
+        int sub=1;
+        while (a>=0) {
+            System.out.println(a);
+            a=a-sub;
+            sub=sub+1;
+
+        }
+    }
+}
