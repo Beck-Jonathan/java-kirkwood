330eea2a3c1deafab0cc77ed8544a6edc69aa1e5
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-28 19:32:18 -0600

    Feb 28 notes

--- CourseDemos/src/chapter_5_6/feb_28.java
+++ CourseDemos/src/chapter_5_6/feb_28.java
@@ -4,15 +4,8 @@ import java.util.Scanner;
 
 public class feb_28 {
     public static void main(String[] args) {
-        int a = 21;
-        int sub = 1;
-        while (a >= 0) {
-            System.out.println(a);
-            a = a - sub;
-            sub ++;
-            Scanner getter = new Scanner(System.in);
-            int x = getter.nextInt();
 
-        }
+
     }
 }
+
