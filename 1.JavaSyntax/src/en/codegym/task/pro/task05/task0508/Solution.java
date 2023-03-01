395fa14d90ffc3e1f38676a3226f8aaca6b65199
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-28 21:08:32 -0600

    working on CodeGym lvl5

--- 1.JavaSyntax/src/en/codegym/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/en/codegym/task/pro/task05/task0508/Solution.java
@@ -2,7 +2,7 @@ package en.codegym.task.pro.task05.task0508;
 
 import java.util.Scanner;
 
-/* 
+/*
 Remove duplicate strings
 */
 
@@ -10,16 +10,17 @@ public class Solution {
     public static String[] strings;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         strings = new String[6];
-        Scanner getter = new Scanner(System.in);
-        for (int i = 0; i < 6; i++) {
-            strings[i] = getter.nextLine();
+        for(int i = 0; i < 6; i++) {
+            strings[i] = scanner.nextLine();
         }
-        for (int i = 0; i < 5; i++) {
-            for (int j = i; j < 6; j++)
-                if (strings[i] == strings[j]){
-                strings[i] = null;
-                strings[j] = null;
+        for(int i = 0; i < strings.length - 1; i++) {
+            for(int j = i + 1; j < strings.length; j++) {
+                if(strings[i].equals(strings[j])) {
+                    strings[i] = null;
+                    strings[j] = null;
+                }
             }
         }
         for (int i = 0; i < strings.length; i++) {
