0f2541850224313e28c63b06ab1e14181e859290
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-28 21:31:39 -0600

    notes on multi dimensional arrays

--- CourseDemos/src/chapter_5_6/feb_28.java
+++ CourseDemos/src/chapter_5_6/feb_28.java
@@ -6,49 +6,58 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class feb_28 {
-    public static void main(String[] args) {
-        //differences between java arrayas and python lists
-        //jave requires you to specify the data type
-        // Java requires all of them to be the same data type
-
-        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        Object[] stuff = {"Hello", 100};
-        System.out.println(stuff[1]);
-        int lottonumbers[] = new int[6];
-        for (int i = 0; i < 5; i++) {
-            lottonumbers[i] = GenLotto(1, 73);
-        }
-        lottonumbers[5]=GenLotto(1,40);
-
-        System.out.println(Arrays.toString(lottonumbers));
-
-
-        int [ ] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        a[5] = a[3] + a[0];
-        System.out.println("a 5 is "+a[5]);
-
-        //java arrays are fixed length
-        for (int i=0;i<lottonumbers.length-1;i++){
-            System.out.println(lottonumbers[i]);
-        }
-        System.out.println("The PowerBall is " + lottonumbers[lottonumbers.length-1]);
-        // there are two ways to loop. Above is a count controller loop
-        //below a for each loop
-        // data type, followed by variable name, colon, array name
-        for(int lottonumber : lottonumbers){
-            System.out.println(lottonumber);
-        }
-
-    }
-
-    private static int GenLotto(int min, int max) {
+    private static int getRandomNum(int min, int max) {
         Random random = new Random();
-
         int randomNumber = random.nextInt(max + 1 - min) + min;
         return randomNumber;
     }
 
+    public static void main(String[] args) {
+        // Differences between Java arrays and Python lists
+        // Java requires you to specify the data type, Java only allows data of the same type
+        // The square brackets can be placed after the data type or after the variable name
+        // Java arrays are fixed length
+        Object[] stuff = {"Hello", 100};
+        String daysOfWeek[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println(daysOfWeek[3]); // Wednesday
+        int lottoNumbers[] = new int[6];
+        lottoNumbers[0] = getRandomNum(1,75);
+        lottoNumbers[1] = getRandomNum(1,75);
+        lottoNumbers[2] = getRandomNum(1,75);
+        lottoNumbers[3] = getRandomNum(1,75);
+        lottoNumbers[4] = getRandomNum(1,75);
+        lottoNumbers[5] = getRandomNum(1,40);
+        // Printing arrays is much more complicated
+        System.out.println(lottoNumbers);
+        System.out.println(Arrays.toString(lottoNumbers));
+        // There are two ways to loop through arrays
+        // count-controlled loop
+        for(int i = 0; i < lottoNumbers.length - 1; i++) {
+            System.out.println(lottoNumbers[i]);
+        }
+        System.out.println("The power ball is: " + lottoNumbers[lottoNumbers.length - 1]);
+        // for-each loop
+        // (DataType variableName: arrayName)
+        int count = 0;
+        for(int num: lottoNumbers) {
+            System.out.println(num);
+        }
 
+        String[][] stateCapitals = {
+                {"Iowa", "Des Moines"},
+                {"Nebraska", "Lincoln"},
+                {"Wisconsin", "Madison"}
+        };
+        for(String[] row: stateCapitals) {
+            for(String cell: row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < stateCapitals.length; i++) {
+            String state = stateCapitals[i][0];
+            String capital = stateCapitals[i][1];
+            System.out.printf("The capital of %s is %s\n", state, capital);
+        }
+    }
 }
-
-
