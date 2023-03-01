fdd66483b9e316fe8d04e8a840ac4da9d70069fe
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-02-28 20:50:51 -0600

    working on CodeGym lvl5

--- 1.JavaSyntax/src/en/codegym/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/en/codegym/task/pro/task05/task0508/Solution.java
@@ -10,7 +10,18 @@ public class Solution {
     public static String[] strings;
 
     public static void main(String[] args) {
-        //write your code here
+        strings = new String[6];
+        Scanner getter = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            strings[i] = getter.nextLine();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 6; j++)
+                if (strings[i] == strings[j]){
+                strings[i] = null;
+                strings[j] = null;
+            }
+        }
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
         }
