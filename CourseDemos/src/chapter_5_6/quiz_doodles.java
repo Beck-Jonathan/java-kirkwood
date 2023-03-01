a697addb39d713c31fc8e040363adee30f54d678
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-03-01 15:27:38 -0600

    testing token status

--- CourseDemos/src/chapter_5_6/quiz_doodles.java
+++ CourseDemos/src/chapter_5_6/quiz_doodles.java
@@ -2,6 +2,6 @@ package chapter_5_6;
 
 public class quiz_doodles {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
