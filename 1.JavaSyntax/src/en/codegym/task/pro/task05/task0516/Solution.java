d759a7230309e071059311d00bc1c25730aa1016
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-03-07 18:39:00 -0600

    0516 to match in class lab

--- 1.JavaSyntax/src/en/codegym/task/pro/task05/task0516/Solution.java
+++ 1.JavaSyntax/src/en/codegym/task/pro/task05/task0516/Solution.java
@@ -1,26 +1,24 @@
 package en.codegym.task.pro.task05.task0516;
-
 import java.util.Arrays;
 
-/* 
+/*
 Filling an array
 */
 
 public class Solution {
 
-    public static int[] array = new int[20];
+    public static int[] array = new int[9];
     public static int valueStart = 10;
     public static int valueEnd = 13;
 
     public static void main(String[] args) {
-        if (array.length % 2 == 0) {
-            Arrays.fill(array, 0, array.length / 2, valueStart);
-            Arrays.fill(array, array.length / 2, array.length, valueEnd);
-        } else {
-            Arrays.fill(array, 0, array.length / 2 + 1, valueStart);
-            Arrays.fill(array, array.length / 2 + 1, array.length, valueEnd);
+        int stopValue = array.length / 2;
+        if(array.length % 2 != 0) {
+            stopValue++;
         }
-
+        Arrays.fill(array, 0, stopValue, valueStart);
+        Arrays.fill(array, stopValue, array.length, valueEnd);
         System.out.println(Arrays.toString(array));
     }
 }
+
