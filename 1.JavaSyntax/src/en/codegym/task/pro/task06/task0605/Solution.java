c8445bcff20389dc73c3409fca89efee7c08cd93
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-03-21 18:29:33 -0500

    updated task 0605

--- 1.JavaSyntax/src/en/codegym/task/pro/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/en/codegym/task/pro/task06/task0605/Solution.java
@@ -10,13 +10,13 @@ import java.util.Collections;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         printArray(array);
         reverseArray(array);
         printArray(array);
     }
 
-    public static void reverseArray(int[] array) {
+    public static void reverseArray(Integer[] array) {
         for (int i = 0; i < array.length / 2; i++) {
            // int holder = array[i];
             //array[i] = array[array.length - i - 1];
@@ -25,7 +25,7 @@ public class Solution {
         }
     }
 
-    public static void printArray(int[] array) {
+    public static void printArray(Integer[] array) {
         for (int i : array) {
             System.out.print(i + ", ");
         }
