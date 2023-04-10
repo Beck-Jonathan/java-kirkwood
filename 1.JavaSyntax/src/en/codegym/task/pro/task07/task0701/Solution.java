34c17b8c0056e24fb39fab1dbea471f54785a2f2
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-04-10 11:24:11 -0500

    task 08 complete

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.pro.task07.task0701;
+
+/* 
+All possible types
+*/
+
+public class Solution {
+    byte x;
+    short a;
+    int b;
+    long c;
+    float d;
+    double e;
+    boolean f;
+    char g;
+}
---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package en.codegym.task.pro.task07.task0702;
+
+/* 
+Maximum integer
+*/
+
+public class Solution {
+    byte l = 127;
+    short m = 32767;
+    int n = Integer.MAX_VALUE;
+    long o = Long.MAX_VALUE;
+}
