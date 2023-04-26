50e1d5c824602e41ae79051cbc0c9f7e35667d72
JONATHAN BECK <jonathan-beck1@student.kirkwood.edu>
2023-04-25 21:13:35 -0500

    implemented searrch method in final project

--- final_proj/src/main/java/Tasks/Find.java
+++ final_proj/src/main/java/Tasks/Find.java
@@ -13,13 +13,14 @@ public class Find implements TaskHandler {
     @Override
     public void handleTask(Scanner scanner) {
         ArrayList<Zelda_Game> games = ZeldaDAO.getAllGames();
-        ArrayList <Zelda_Game> copy = Helpers.cloneList(games);
+
         while(true) {
+            ArrayList <Zelda_Game> copy = Helpers.cloneList(games);
             System.out.println("** find menu **");
 
             String search = UserInput.getString(scanner, "Enter the game's title, or 00 to exit");
             if (search.equals("00")){break;}
-            copy.removeIf((Zelda_Game -> !Zelda_Game.getName().contains(search)));
+            copy.removeIf((Zelda_Game -> !Zelda_Game.getName().toUpperCase().contains(search.toUpperCase())));
             if (copy.size() > 0) {
                 Helpers.PrintTableHeaderRow();
                 for (Zelda_Game game : copy) {
